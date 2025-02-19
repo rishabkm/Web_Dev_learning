04cb924b016b825238d417734ae85ef5da4089bd
rishabkm <119279619+rishabkm@users.noreply.github.com>
2025-02-19 22:10:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("LoginForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent page reload
+    console.log("Form submitted!"); // Check if it's working
+});
